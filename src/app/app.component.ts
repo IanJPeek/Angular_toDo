021d389a6474b0b466522827a408be999b5f2473
IanJPeek <IanJPeek@gmail.com>
2020-01-29 15:28:59 +0000

    demo of simple text insertion/ display with changeName function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang-todolist';
+  name:string = 'Schnuckums';
+
+
+  constructor(){
+    this.name = "bob"
+  }
+
+  changeName(name:string):void {
+    this.name = name
+  }
 }
